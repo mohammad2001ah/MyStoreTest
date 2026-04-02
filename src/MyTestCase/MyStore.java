ed7a403906b61444671aa2528b9e7918d7eace34
mohammad2001ah <mah.2001.ah@gmail.com>
2026-04-02 15:35:19 +0300

    update2/4

--- src/MyTestCase/MyStore.java
+++ src/MyTestCase/MyStore.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -17,44 +18,47 @@ import org.testng.annotations.Test;
 import com.google.common.annotations.VisibleForTesting;
 
 public class MyStore {
-	
-	WebDriver driver=new ChromeDriver();
+
+	WebDriver driver = new ChromeDriver();
 	Random rand = new Random();
+
 	@BeforeTest
 	public void MySetUP() {
 		driver.get("http://localhost:3000/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
-	@Test(priority = 1)
+
+	@Test(priority = 1, enabled = false)
 	public void OpenSingUpPage() throws InterruptedException {
-		WebElement DropDown=driver.findElement(By.className("dropdown"));
+		WebElement DropDown = driver.findElement(By.className("dropdown"));
 		DropDown.click();
 		Thread.sleep(2000);
-		List<WebElement> SingUP=driver.findElements(By.className("dropdown-item"));
+		List<WebElement> SingUP = driver.findElements(By.className("dropdown-item"));
 		SingUP.get(1).click();
 	}
-	@Test(priority = 2)
+
+	@Test(priority = 2, enabled = false)
 	public void CreateUser() throws InterruptedException {
-		//WebElement
-		WebElement Name= driver.findElement(By.id("signup-name"));
-		WebElement Email=driver.findElement(By.id("signup-email"));
-		WebElement Password=driver.findElement(By.id("signup-password"));
-		WebElement ConfirmPassword=driver.findElement(By.id("signup-confirm-password"));
+		// WebElement
+		WebElement Name = driver.findElement(By.id("signup-name"));
+		WebElement Email = driver.findElement(By.id("signup-email"));
+		WebElement Password = driver.findElement(By.id("signup-password"));
+		WebElement ConfirmPassword = driver.findElement(By.id("signup-confirm-password"));
 		Thread.sleep(3000);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollTo(0,500)");
-		WebElement CustomerRole=driver.findElement(By.id("label-role-customer"));
-		WebElement SingUpSubmit=driver.findElement(By.xpath("//button[@data-testid='testid-signup-submit']"));
-		
-		//Action
+		WebElement CustomerRole = driver.findElement(By.id("label-role-customer"));
+		WebElement SingUpSubmit = driver.findElement(By.xpath("//button[@data-testid='testid-signup-submit']"));
+
+		// Action
 		Name.sendKeys("TestUser1");
 		Email.sendKeys("test1@gmail.com");
 		Password.sendKeys("Password123@");
 		ConfirmPassword.sendKeys("Password123@");
 		CustomerRole.click();
 		SingUpSubmit.click();
-		
+
 	}
 
 	@Test(priority = 3, enabled = true)
@@ -84,13 +88,14 @@ public class MyStore {
 		// WebElement
 		WebElement MyProduct = driver.findElement(By.id("nav-products"));
 		MyProduct.click();
+
 		String[] productNames = { "Laptop", "Mobile Phone", "Headphones", "Keyboard", "Mouse", "Monitor", "Tablet",
 				"Smartwatch", "Printer", "USB Flash" };
 		String[] productPrices = { "1200", "800", "150", "70", "40", "300", "500", "200", "250", "25" };
 
 		String[] productStocks = { "5", "12", "25", "40", "60", "10", "8", "18", "7", "100" };
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i <= productNames.length; i++) {
 			int randomeInfo = rand.nextInt(productNames.length);
 			Thread.sleep(1000);
 			WebElement NameProduct = driver.findElement(By.id("input-product-name"));
@@ -110,29 +115,24 @@ public class MyStore {
 
 	@Test(priority = 5, enabled = true)
 	public void DeleteProductRandom() throws InterruptedException {
-		// WebElement
 		WebElement MyProduct = driver.findElement(By.id("nav-products"));
 		MyProduct.click();
-		
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		Thread.sleep(2000);
-		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-		
 		WebElement TheTable = driver.findElement(By.id("products-list-table"));
-	    List<WebElement> deleteButton= driver.findElements(By.className("delete-btn"));
-		boolean flag=true;
-		int count =0;
-		while(flag) {
-			if(deleteButton.size()>0) {
-				Thread.sleep(1000);
-				deleteButton.get(count).click();
+		boolean flag = true;
+		while (flag) {
+			List<WebElement> deleteButton = driver.findElements(By.className("delete-btn"));
+			if (deleteButton.size() > 0) {
+				Thread.sleep(2000);
+				js.executeScript("arguments[0].scrollIntoView(true);", deleteButton.get(0));
+				Thread.sleep(500);
+				deleteButton.get(0).click();
 				Alert alert = driver.switchTo().alert();
 				alert.accept();
+				Thread.sleep(3000);
 				System.out.println("Is Removed");
-				count++;
-			}
-			else {
-				break;
+			} else {
+				flag = false;
 			}
 		}
 		Thread.sleep(1500);
